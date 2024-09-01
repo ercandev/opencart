81386d267adc22a71f133e7f77008400c05bc6ca
Ercan SORMAZ <ercansormaz@gmail.com>
2024-09-01 10:02:10 +0300

    Turkish Language Add

---
+++ admin/language/tr-tr/common/footer.php
@@ -0,0 +1,6 @@
+<?php
+/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
+
+// Text
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Tüm Hakları Saklıdır.';
+$_['text_version'] = 'Sürüm %s';
